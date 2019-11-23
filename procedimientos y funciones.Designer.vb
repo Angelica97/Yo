94647827da008e9266250208cc0a70a96c25aa58
Angelica97 <57775842+Angelica97@users.noreply.github.com>
2019-11-22 23:31:25 -0400

    Add files via upload

---
+++ procedimientos y funciones.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnAperturarCuenta = New System.Windows.Forms.Button()
+        Me.txtCliente = New System.Windows.Forms.TextBox()
+        Me.txtMonto = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnDepósitos = New System.Windows.Forms.Button()
+        Me.btnRetiros = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.lstDepósitos = New System.Windows.Forms.ListBox()
+        Me.lstRetiros = New System.Windows.Forms.ListBox()
+        Me.txtSaldo = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.btnAperturarCuenta)
+        Me.GroupBox1.Controls.Add(Me.txtCliente)
+        Me.GroupBox1.Controls.Add(Me.txtMonto)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(705, 220)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingreso de Datos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 133)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Monto"
+        '
+        'btnAperturarCuenta
+        '
+        Me.btnAperturarCuenta.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAperturarCuenta.Location = New System.Drawing.Point(435, 84)
+        Me.btnAperturarCuenta.Name = "btnAperturarCuenta"
+        Me.btnAperturarCuenta.Size = New System.Drawing.Size(135, 33)
+        Me.btnAperturarCuenta.TabIndex = 7
+        Me.btnAperturarCuenta.Text = "Aperturar Cuenta"
+        Me.btnAperturarCuenta.UseVisualStyleBackColor = True
+        '
+        'txtCliente
+        '
+        Me.txtCliente.Location = New System.Drawing.Point(96, 59)
+        Me.txtCliente.Name = "txtCliente"
+        Me.txtCliente.Size = New System.Drawing.Size(203, 29)
+        Me.txtCliente.TabIndex = 3
+        '
+        'txtMonto
+        '
+        Me.txtMonto.Location = New System.Drawing.Point(96, 133)
+        Me.txtMonto.Name = "txtMonto"
+        Me.txtMonto.Size = New System.Drawing.Size(203, 29)
+        Me.txtMonto.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(24, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cliente"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Gray
+        Me.GroupBox2.Controls.Add(Me.btnDepósitos)
+        Me.GroupBox2.Controls.Add(Me.btnRetiros)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 226)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(705, 109)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Operaciones"
+        '
+        'btnDepósitos
+        '
+        Me.btnDepósitos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDepósitos.Location = New System.Drawing.Point(112, 57)
+        Me.btnDepósitos.Name = "btnDepósitos"
+        Me.btnDepósitos.Size = New System.Drawing.Size(153, 35)
+        Me.btnDepósitos.TabIndex = 9
+        Me.btnDepósitos.Text = "Depósitos"
+        Me.btnDepósitos.UseVisualStyleBackColor = True
+        '
+        'btnRetiros
+        '
+        Me.btnRetiros.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRetiros.Location = New System.Drawing.Point(381, 57)
+        Me.btnRetiros.Name = "btnRetiros"
+        Me.btnRetiros.Size = New System.Drawing.Size(157, 35)
+        Me.btnRetiros.TabIndex = 8
+        Me.btnRetiros.Text = "Retiros"
+        Me.btnRetiros.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.Color.LightCoral
+        Me.GroupBox3.Controls.Add(Me.btnLimpiar)
+        Me.GroupBox3.Controls.Add(Me.lstDepósitos)
+        Me.GroupBox3.Controls.Add(Me.lstRetiros)
+        Me.GroupBox3.Controls.Add(Me.txtSaldo)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(0, 351)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(705, 185)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Historial"
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.Location = New System.Drawing.Point(562, 123)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(123, 39)
+        Me.btnLimpiar.TabIndex = 10
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'lstDepósitos
+        '
+        Me.lstDepósitos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDepósitos.FormattingEnabled = True
+        Me.lstDepósitos.ItemHeight = 20
+        Me.lstDepósitos.Location = New System.Drawing.Point(96, 78)
+        Me.lstDepósitos.Name = "lstDepósitos"
+        Me.lstDepósitos.Size = New System.Drawing.Size(120, 84)
+        Me.lstDepósitos.TabIndex = 5
+        '
+        'lstRetiros
+        '
+        Me.lstRetiros.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstRetiros.FormattingEnabled = True
+        Me.lstRetiros.ItemHeight = 20
+        Me.lstRetiros.Location = New System.Drawing.Point(312, 78)
+        Me.lstRetiros.Name = "lstRetiros"
+        Me.lstRetiros.Size = New System.Drawing.Size(120, 84)
+        Me.lstRetiros.TabIndex = 6
+        '
+        'txtSaldo
+        '
+        Me.txtSaldo.Location = New System.Drawing.Point(576, 83)
+        Me.txtSaldo.Name = "txtSaldo"
+        Me.txtSaldo.Size = New System.Drawing.Size(100, 29)
+        Me.txtSaldo.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(596, 60)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 20)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Saldo"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(341, 37)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Retiros"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(117, 37)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Depositos"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(821, 574)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnAperturarCuenta As System.Windows.Forms.Button
+    Friend WithEvents txtCliente As System.Windows.Forms.TextBox
+    Friend WithEvents txtMonto As System.Windows.Forms.TextBox
+    Friend WithEvents btnDepósitos As System.Windows.Forms.Button
+    Friend WithEvents btnRetiros As System.Windows.Forms.Button
+    Friend WithEvents btnLimpiar As System.Windows.Forms.Button
+    Friend WithEvents lstDepósitos As System.Windows.Forms.ListBox
+    Friend WithEvents lstRetiros As System.Windows.Forms.ListBox
+    Friend WithEvents txtSaldo As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ procedimientos y funciones.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+
+    Private Sub activarControles()
+        txtCliente.Enabled = False
+        txtMonto.Enabled = False
+        btnAperturarCuenta.Enabled = False
+        btnRetiros.Enabled = True
+        btnDepósitos.Enabled = True
+    End Sub
+
+    Private Sub desactivarControles()
+        txtCliente.Enabled = True
+        txtMonto.Enabled = True
+        btnAperturarCuenta.Enabled = True
+        btnRetiros.Enabled = False
+        btnDepósitos.Enabled = False
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        desactivarControles()
+    End Sub
+    Private monto As Double
+
+
+    Private Sub btnAperturarCuenta_Click(sender As Object, e As EventArgs) Handles btnAperturarCuenta.Click
+
+        Dim cliente As String
+        cliente = txtCliente.Text
+        monto = Val(txtMonto.Text)
+        If (monto > 0) Then
+            activarControles()
+        Else
+            MessageBox.Show("El monto no puede ser menor que cero", "Gestión de ahorros,", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Function leer(mensaje As String) As Double
+        Dim cantidad As Double
+        cantidad = InputBox("Ingrese monto a" + mensaje, "Gestion de ahorros", "0", 100, 0)
+        Return cantidad
+    End Function
+    Private Sub mostrar()
+        txtSaldo.Text = monto
+    End Sub
+
+    Private Sub btnDepósitos_Click(sender As Object, e As EventArgs) Handles btnDepósitos.Click
+        Dim Depositos As Double
+        Depositos = leer("depositar")
+        monto = monto + Depositos
+        lstDepósitos.Items.Add(Depositos)
+        mostrar()
+    End Sub
+
+    Private Sub btnRetiros_Click(sender As Object, e As EventArgs) Handles btnRetiros.Click
+        Dim Retiros As Double
+        Retiros = leer("Retirar")
+        If (Retiros <= monto) Then
+            monto = monto - Retiros
+            lstRetiros.Items.Add(Retiros)
+            mostrar()
+        Else
+            MessageBox.Show("No se puede retirar una cantidad mayor al monto actual", "gestion de ahorros", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End If
+
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        desactivarControles()
+        lstDepósitos.Items.Clear()
+        lstRetiros.Items.Clear()
+        txtMonto.Clear()
+        txtSaldo.Clear()
+        txtCliente.Clear()
+
+    End Sub
+End Class
